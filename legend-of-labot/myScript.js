b83e1378a7a6748d8613f301d05dd403ec3c12a1
naknamu <jodeldelvalle@gmail.com>
2022-02-27 15:02:28 +0800

    Added js file

---
+++ legend-of-labot/myScript.js
@@ -0,0 +1,4 @@
+function openSteamStore(){
+    //open steam store page of my game legend of labot
+    window.open("https://store.steampowered.com/app/1753160/Legend_of_Labot_The_Golden_Pearl/");
+}
